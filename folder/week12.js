04a3c75b7952dd7e5b998e27837569fe246476fd
nimafaghihzadeh <83329582+nimafaghihzadeh@users.noreply.github.com>
2021-05-05 11:34:48 +1000

    Add files via upload

---
+++ folder/week12.js
@@ -0,0 +1,14 @@
+function swapBoxes() {
+    console.log ('idk what it is (now ik, it is only to see that js is working');
+
+    if(boxone.style.background == 'red') {
+        boxone.style.background ='black';
+        boxtwo.style.background ='red';
+    }
+    else {
+        boxone.style.background = 'red';
+        boxtwo.style.background = 'black';
+     
+    
+    }
+}
